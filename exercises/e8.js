3e011739e7188a9e82a99b25d2c9f15b21e915e9
michael <michaelsanchez8815@gmail.com>
2022-12-30 13:22:53 -0600

    revise e8

--- exercises/e8.js
+++ exercises/e8.js
@@ -4,15 +4,29 @@ import { data } from '../data/data';
 // Return a Planet name by a given moon name
 // Return example: 'Planet Name'
 
+// export function findPlanetNameByMoon(data, moonName) {
+//   // Your code goes here...
+//   const planetsArray = data.planets.filter((planet) => {
+//     if (planet.moons) {
+//       return planet.moons.find((moon) => moon === moonName);
+//     }
+//     return false;
+//   });
+//   return planetsArray[0].name;
+// }
+
 export function findPlanetNameByMoon(data, moonName) {
   // Your code goes here...
-  const planetsArray = data.planets.filter((planet) => {
-    if (planet.moons) {
-      return planet.moons.find((moon) => moon === moonName);
-    }
-    return false;
-  });
-  return planetsArray[0].name;
+  return data.planets.find((planet) => {
+    return planet.moons?.find?.((moon) => moon === moonName);
+  })?.name;
+  // const planetsArray = data.planets.filter((planet) => {
+  //   if (planet.moons) {
+  //     return planet.moons.find((moon) => moon === moonName);
+  //   }
+  //   return false;
+  // });
+  // return planetsArray[0].name;
 }
 
 // === TEST YOURSELF ===
